2ab70784f6b957144fc7d29c366b97533159c29d
vikrammadhikunta <bhargavi3399@iiitmanipur.ac.in>
2025-09-13 21:07:39 +0530

    first commit

---
+++ server/connectMongoDb.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.CONNECTION_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`✅ MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
